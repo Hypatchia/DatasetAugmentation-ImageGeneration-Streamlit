684f4668574846323a7029fa834e282e1fd81722
Samia Belhaddad <samiamagbelhaddad@gmail.com>
2023-11-19 00:43:05 +0100

    Deploy

---
+++ app.py
@@ -0,0 +1,66 @@
+import streamlit as st
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import os
+from io import BytesIO
+import zipfile
+import base64
+
+# Load the generator and discriminator models
+generator =  tf.saved_model.load("Generator")
+save_dir = "generated_images"  # Directory to save the generated images
+
+def generate_and_save_images(generator,num_images=1000, save_dir=save_dir):
+    noise_dim=50
+    # Generate random noise for the specified number of images
+    noise = tf.random.normal([num_images, noise_dim])
+
+    # Generate images using the generator
+    generated_images = generator(noise, training=False)
+
+    # Create the directory if it does not exist
+    os.makedirs(save_dir, exist_ok=True)
+
+    # Calculate the number of rows and columns for the subplot grid
+    rows = cols = int(num_images**0.5)
+    while rows * cols < num_images:
+        cols += 1
+
+    # Save the generated images in a grid
+    plt.figure(figsize=(15, 15))
+    for i in range(num_images):
+        plt.subplot(rows, cols, i + 1)
+        plt.imshow(generated_images[i, :, :, 0], cmap='gray')
+        plt.axis('off')
+        # Save the generated image
+        image_path = os.path.join(save_dir, f'generated_image_{i}.png')
+        plt.imsave(image_path, generated_images[i, :, :, 0].numpy(), cmap='gray')
+
+    plt.close()
+
+
+    st.write("Generation Done, Download the images")
+
+    return save_dir, num_images
+
+# Streamlit app
+st.title("Image Generator App")
+# Allow the user to input the number of images manually
+num_images = st.number_input("Enter the number of images to generate", min_value=1, value=10, step=1)
+
+if st.button("Generate Images"):
+    save_dir, num_images = generate_and_save_images(generator, num_images)
+    st.success(f"Images generated and saved in {save_dir}")
+
+    # Zip the generated images
+    zip_buffer = BytesIO()
+    with zipfile.ZipFile(zip_buffer, 'a', zipfile.ZIP_DEFLATED) as zip_file:
+        for i in range(num_images):
+            image_path = os.path.join(save_dir, f'generated_image_{i}.png')
+            zip_file.write(image_path, f'generated_image_{i}.png')
+
+    # Download the zip file
+    st.markdown("### Download Generated Images")
+    st.markdown(
+        f"Click the link below to download the zip file containing the generated images: [Download Zip File](data:application/zip;base64,{base64.b64encode(zip_buffer.getvalue()).decode()})"
+    )
---
+++ readme.md
@@ -0,0 +1,37 @@
+## Overview
+
+This project is about building a streamlit app to integrate a trained Generator from a GAN to generate realistic images of a Medical Dataset of Tuberculosis, a minority class in the X ray-Chest Dataset.
+
+* You can Generate Any Number of Images from the Tuberculosis Class by using the streamlit app on this link:
+<a></a>
+* All you have to do set a number of images to generate and click a button to generate in real time & download a zip file of the images you created.
+
+## Image Dataset Augmentation using GANs
+
+By leveraging the capabilities of GANs, our augmentation technique is now able to generate synthetic images seamlessly integrated with the original dataset.
+The objective is to broaden the project and offer the ability to try the generator to generate any number of images a user wants.
+The integration of GAN-based augmentation is poised to elevate model performance and adaptability.
+
+Explore the Dataset Augmentation Repository here <a> </a> to delve into the full implementation of the Deep Convolutional Generative Adversarial Network.
+
+## Steps to run 
+
+* Always Create a Virtual Environment (I am watching u, do it)
+~~~
+python -m venv "name of env"
+~~~
+* Activate environement
+~~~
+Source path to /Scripts/activate
+~~~
+* Install requirements.txt in environment
+~~~
+pip install -r requirements.txt
+~~~
+
+* Run streamlit app
+~~~
+streamlit run app.py
+~~~
+
+## Contact
